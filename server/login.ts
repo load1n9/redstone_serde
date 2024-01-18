e24f3ef11b2516d4a94dbde828385b1cb08482a0
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2024-01-18 09:25:56 -0500

    feat: varintstring

--- server/login.ts
+++ server/login.ts
@@ -1,9 +1,9 @@
 import { Struct, type InnerType} from "../deps.ts";
-import { varIntString } from "../util_classes.ts";
+import { varIntString, u128 } from "../util_classes.ts";
 
 export const loginStartCodec = new Struct({
     name: varIntString,
-    // uuid: ,
+    uuid: u128,
 });
 
 
--- util_classes.ts
+++ util_classes.ts
@@ -20,4 +20,28 @@ export class VarIntString extends UnsizedType<string> {
 }
 
 
+
+
+/**
+ * Encoded as an unsigned 128-bit integer (or two unsigned 64-bit integers: the most significant 64 bits and then the least significant 64 bits)
+*/
+export class U128 extends UnsizedType<bigint> {
+    constructor() {
+        super(16);
+    }
+    
+    readPacked(dt: DataView, options: Options = { byteOffset: 0}): bigint {
+        const high = dt.getBigUint64(options.byteOffset, false);
+        const low = dt.getBigUint64(options.byteOffset + 8, false);
+        return (high << 64n) + low;
+    }
+    
+    writePacked(value: bigint, dt: DataView, options: Options = { byteOffset: 0}): void {
+        dt.setBigUint64(options.byteOffset, value >> 64n, false);
+        dt.setBigUint64(options.byteOffset + 8, value & 0xFFFFFFFFFFFFFFFFn, false);
+    }
+}
+
 export const varIntString = new VarIntString();
+export const u128 = new U128();
+
